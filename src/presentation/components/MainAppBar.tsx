e2c2d1a54dea53b6307aefefc0771c4a9ca2114b
Maksim Zayakin <rasetsuq@yandex.ru>
2025-09-13 15:16:34 +0500

    feat: add redirect to homepage in appbar

--- src/presentation/components/MainAppBar.tsx
+++ src/presentation/components/MainAppBar.tsx
@@ -1,12 +1,18 @@
 import { AppBar, Toolbar, Typography, Box } from "@mui/material";
 import AuthMenu from "./AuthMenu";
+import { useNavigate } from "react-router-dom";
 
 const MainAppBar = () => {
+  const navigate = useNavigate();
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          UniJobs
+        <Typography
+          variant="h6"
+          sx={{ flexGrow: 1, cursor: "pointer" }}
+          onClick={() => navigate("/")}
+        >
+          UniJob
         </Typography>
         <Box>
           <AuthMenu />
