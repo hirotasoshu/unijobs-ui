aeb5bccc2b5f37b75a8cba748aac9fa17141e876
Maksim Zayakin <rasetsuq@yandex.ru>
2025-09-15 01:59:55 +0500

    feat: add employer http gateway

---
+++ src/infra/persistance/http/employerGateway.ts
@@ -0,0 +1,27 @@
+import { EmployerDetailViewModel } from "../../../application/common/models/employer";
+import { EmployerGateway } from "../../../application/query/ports/employerGateway";
+import { EmployerId } from "../../../domain/valueObjects/id";
+
+export class HttpEmployerGateway implements EmployerGateway {
+  private baseUrl: string;
+
+  constructor(baseUrl: string = "http://127.0.0.1:8000/employers") {
+    this.baseUrl = baseUrl;
+  }
+
+  async getById(id: EmployerId): Promise<EmployerDetailViewModel> {
+    const response = await fetch(`${this.baseUrl}/${id}`);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const responseJson = await response.json();
+    return {
+      id: responseJson.id,
+      name: responseJson.name,
+      avatarUrl: responseJson.avatar_url,
+      description: responseJson.description,
+    };
+  }
+}
